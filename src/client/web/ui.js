4f7a8ed66730075aa85663a501a095483023ae70
Mattias <muthaias.uu@gmail.com>
2021-04-29 00:24:00 +0200

    Add initial theme support

--- src/client/web/ui.js
+++ src/client/web/ui.js
@@ -1,8 +1,13 @@
 var controlPosition = "main menu"; // || side menu
+var themes = [
+    "base",
+    "dark"
+]
 function resize() {
     const canvas = document.getElementById("canv");
     const controlDiv = document.getElementById("control");
     const contributeDiv = document.getElementById("contribute");
+    const themeDiv = document.getElementById("theme");
     canvas.width = canvas.offsetWidth;
     canvas.height = canvas.offsetHeight;
 
@@ -10,12 +15,36 @@ function resize() {
         controlDiv.classList.add("menu")
         controlDiv.classList.remove("game")
         contributeDiv.classList.remove("hidden")
+        themeDiv.classList.remove("hidden")
     } else {
         controlDiv.classList.remove("menu")
         controlDiv.classList.add("game")
         contributeDiv.classList.add("hidden")
+        themeDiv.classList.add("hidden")
     }
 }
+
+function setTheme(themeId) {
+    const theme = document.getElementById("theme");
+    theme.innerHTML = `
+<select id="theme-select">
+    ${themes.map(t => `<option value="${t}" ${t === themeId ? "selected" : ""}>${t}</option>`).join("\n")}
+</select>
+    `
+    const themeSelect = document.getElementById("theme-select")
+    themeSelect.onchange = handleSetTheme
+    const body = document.getElementById("main")
+    for (const t of themes) {
+        body.classList.remove("theme-" + t)
+    }
+    body.classList.add("theme-" + themeId)
+    localStorage.setItem("themeId", themeId)
+}
+
+function handleSetTheme(e) {
+    setTheme(e.target.value)
+}
+
 function start() {
     // Prevent right-click menu on canvas.
     const canvas = document.getElementById("canv");
@@ -24,6 +53,7 @@ function start() {
             e.preventDefault();
         }
     });
+    setTheme(localStorage.getItem("themeId") || themes[0])
 
     window.onresize = function () {
         resize();
