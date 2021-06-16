5f27929234cc8384c1417b3fde7caf0c39c3d6b4
Jannis Mohlin Tsiroyannis <jannis.mohlin.tsiroyannis@gmail.com>
2021-06-16 23:32:44 +0200

    Make number printing a bit more consitent.

--- src/client/rendering.c
+++ src/client/rendering.c
@@ -157,25 +157,22 @@ void renderGraph(unsigned nodeFocus, float viewEulerX, float viewEulerY,
 	       var controllingColor = UTF8ToString(playerColors[controlledBy[node]]); 
 	       context.strokeStyle = controllingColor;
 	       context.fillStyle = controllingColor;
-               context.fillText("(" + node + ") " + controllingName, nodes[node*3+0]+5+perspectiveRadius, nodes[node*3+1]+perspectiveRadius);
-               if (strength[node] != 1.0)
-               {
-                  if (orderCount[node] > 1)
-                  {
-                     context.fillText("+ " + orderCount[node] + " * " + strength[node].toFixed(2), nodes[node*3+0]+5+perspectiveRadius, nodes[node*3+1]+14+perspectiveRadius);
-                  }
-                  else
-                  {
-                     context.fillText("+ " + strength[node].toFixed(2), nodes[node*3+0]+5+perspectiveRadius, nodes[node*3+1]+14+perspectiveRadius);
-                  }
-               }
-	       context.fillStyle = controllingColor;
-	    }
+               context.fillText(node + " " + controllingName, nodes[node*3+0]+5+perspectiveRadius, nodes[node*3+1]+perspectiveRadius);
+            }
 	    else
 	    {
 	       context.fillStyle = "black";
-               context.fillText("(" + node + ")", nodes[node*3+0]+5+perspectiveRadius, nodes[node*3+1]+5+perspectiveRadius);
+               context.fillText(node, nodes[node*3+0]+5+perspectiveRadius, nodes[node*3+1]+perspectiveRadius);
 	    }
+            if (orderCount[node] > 1)
+            {
+               context.fillText(orderCount[node] + " * " + strength[node].toFixed(2), nodes[node*3+0]+5+perspectiveRadius, nodes[node*3+1]+14+perspectiveRadius);
+            }
+            else
+            {
+               context.fillText(strength[node].toFixed(2), nodes[node*3+0]+5+perspectiveRadius, nodes[node*3+1]+14+perspectiveRadius);
+            }
+            
 
 	    // Draw the node itself
 	    context.beginPath();
